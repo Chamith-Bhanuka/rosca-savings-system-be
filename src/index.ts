67787ed4da38a843ac94db62ccb8c61e3a99b23a
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-12-02 19:47:29 +0530

    feat: establish MongoDB connection via Mongoose in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
-import express = require("express");
-import dotenv from "dotenv";
+import express = require('express');
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 const SERVER_PORT = process.env.SERVER_PORT;
+const MONGODB_URI = process.env.MONGO_URI as string;
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/',(req, res) => {
-    res.send('Backend is running..!');
-})
+app.use('/', (req, res) => {
+  res.send('Backend is running..!');
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('MongoDB Connected');
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
 
 app.listen(SERVER_PORT, () => {
-    console.log(`Server is running on port ${SERVER_PORT}`);
+  console.log(`Server is running on port ${SERVER_PORT}`);
 });
