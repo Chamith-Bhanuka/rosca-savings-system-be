83184e451d1364f824228121c89adbca7706d5b7
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-12-02 19:46:09 +0530

    feat: add Notification model with MongoDB schema

---
+++ src/model/notification.model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export enum Type {
+  Payment_Reminder = 'PAYMENT_REMINDER',
+  Draw_Result = 'DRAW_RESULT',
+  Dispute_Update = 'DISPUTE_UPDATE',
+  Group_Invite = 'GROUP_INVITE',
+  General = 'GENERAL',
+}
+
+export interface INotification extends Document {
+  _id: mongoose.Types.ObjectId;
+  user: mongoose.Types.ObjectId;
+  group?: mongoose.Types.ObjectId;
+  type: Type;
+  payload?: any;
+  read: boolean;
+  sendAt: Date;
+  createdAt: Date;
+}
+
+const NotificationSchema = new Schema<INotification>({
+  user: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+  group: { type: mongoose.Types.ObjectId, ref: 'Group' },
+  type: { type: String, enum: Object.values(Type), required: true },
+  payload: { type: Schema.Types.Mixed },
+  read: { type: Boolean, default: false },
+  sendAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export const Notification = mongoose.model('Notification', NotificationSchema);
