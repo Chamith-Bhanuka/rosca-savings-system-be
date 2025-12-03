a6cfc2a7e3f99baa9abd0717ab93aa65892a9f41
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-12-03 14:14:58 +0530

    feat: update auth controller with login and get my details handlers

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Request, Response } from 'express';
-import { User } from '../model/user.model';
+import e, { Request, Response } from 'express';
+import { IUser, User } from '../model/user.model';
 import bcrypt from 'bcryptjs';
+import { AuthRequest } from '../middleware/auth.middleware';
+import { signAccessToken, signRefreshToken } from '../utils/token';
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -35,5 +37,73 @@ export const register = async (req: Request, res: Response) => {
     console.error('Registration error ', err);
 
     return res.status(500).json({ message: err.message });
+  }
+};
+
+export const getMe = async (req: AuthRequest, res: Response) => {
+  if (!req.user) {
+    return res.status(401).json({ message: 'Unauthorized.!' });
+  }
+
+  const userId = req.user.sub;
+
+  const user =
+    ((await User.findById(userId).select('-password')) as IUser) || null;
+
+  if (!user) {
+    return res.status(404).json({
+      message: 'User not found!',
+    });
+  }
+
+  const { firstName, lastName, email, role } = user;
+
+  res.status(200).json({
+    message: 'Ok',
+    data: { firstName, lastName, email, role },
+  });
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(401).json({ message: 'Invalid Credentials.!' });
+    }
+
+    const valid = await bcrypt.compare(password, existingUser.password!);
+
+    if (!valid) {
+      return res.status(401).json({ message: 'Invalid Credentials.!' });
+    }
+
+    const accessToken = signAccessToken(existingUser);
+
+    const refreshToken = signRefreshToken(existingUser);
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+      message: 'User logged in successfully.!',
+      data: existingUser.email,
+      role: existingUser.role,
+      accessToken: accessToken,
+    });
+  } catch (err: any) {
+    console.error('Login error', err);
+
+    if (err.code === 11000) {
+      return res.status(401).json({ message: 'Invalid Credentials.!' });
+    }
+
+    res.status(500).json({ message: err.message });
   }
 };
