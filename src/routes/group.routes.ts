61288781cc6e5e2f29a0119c35463e9bb8ecbab8
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-12-11 23:58:59 +0530

    feat: update group.routes to add joinGroup POST route with authentication

--- src/routes/group.routes.ts
+++ src/routes/group.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import { authenticate } from '../middleware/auth.middleware';
-import { createGroup, getAllGroups } from '../controllers/group.controller';
+import {
+  createGroup,
+  getAllGroups,
+  joinGroup,
+} from '../controllers/group.controller';
 
 const router = Router();
 
 router.post('/create', authenticate, createGroup);
 
 router.get('/', getAllGroups);
+
+router.post('/:groupId/join', authenticate, joinGroup);
 
 export default router;
