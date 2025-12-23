16da107b0ada1b1f84016ad3bc411aa8a62ed5da
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-12-23 21:12:23 +0530

    feat: add markRead function in notification.controller to update notification read status

--- src/controllers/notification.controller.ts
+++ src/controllers/notification.controller.ts
@@ -1,6 +1,7 @@
 import { AuthRequest } from '../middleware/auth.middleware';
 import { Response } from 'express';
 import { Notification } from '../model/notification.model';
+import { getIo } from '../server/socket';
 
 export const listNotifications = async (req: AuthRequest, res: Response) => {
   const userId = req.user.sub;
@@ -14,4 +15,24 @@ export const listNotifications = async (req: AuthRequest, res: Response) => {
     .sort({ createdAt: -1 })
     .limit(limit);
   res.json({ notifications });
+};
+
+export const markRead = async (req: AuthRequest, res: Response) => {
+  const userId = req.user.sub;
+  const { id } = req.params;
+  console.log('userID', userId);
+  console.log('id', id);
+  const note = await Notification.findOneAndUpdate(
+    { _id: id, user: userId },
+    { read: true },
+    { new: true }
+  );
+  if (!note) return res.status(404).send('Notification not found.!');
+
+  try {
+    getIo()
+      .to(userId.toString())
+      .emit('notification:update', { id: note._id, read: true });
+  } catch {}
+  res.json({ success: true });
 };
