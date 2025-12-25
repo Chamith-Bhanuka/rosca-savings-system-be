41784b022f6a10de0f3d877cbb3e4e5de79e783b
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-12-25 19:42:40 +0530

    feat: add GET /:groupId route in group.routes with authentication

--- src/routes/group.routes.ts
+++ src/routes/group.routes.ts
@@ -6,6 +6,7 @@ import {
   declineJoinRequest,
   getAllGroups,
   joinGroup,
+  getGroupDetails,
 } from '../controllers/group.controller';
 
 const router = Router();
@@ -13,6 +14,8 @@ const router = Router();
 router.post('/create', authenticate, createGroup);
 
 router.get('/', getAllGroups);
+
+router.get('/:groupId', authenticate, getGroupDetails);
 
 router.post('/:groupId/join', authenticate, joinGroup);
 
