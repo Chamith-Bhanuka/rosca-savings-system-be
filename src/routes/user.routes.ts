7a5de8fc71ffef18c69496b9ce686900384c8a3b
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-12-31 22:04:24 +0530

    feat: add /dashboard route in user.routes with authentication

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,10 @@
 import { authenticate } from '../middleware/auth.middleware';
 import { getWalletData } from '../controllers/user.controller';
 import router from './payment.routes';
+import { getDashboardData } from '../controllers/dashboard.controller';
 
 router.get('/wallet', authenticate, getWalletData);
+
+router.get('/dashboard', authenticate, getDashboardData);
 
 export default router;
