6310fc0da14c724765196b070dcef2cb75844ff1
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-12-31 22:05:41 +0530

    feat: add /analytics route in user.routes with authentication

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,9 +2,12 @@ import { authenticate } from '../middleware/auth.middleware';
 import { getWalletData } from '../controllers/user.controller';
 import router from './payment.routes';
 import { getDashboardData } from '../controllers/dashboard.controller';
+import { getUserAnalytics } from '../controllers/analytics.controller';
 
 router.get('/wallet', authenticate, getWalletData);
 
 router.get('/dashboard', authenticate, getDashboardData);
+
+router.get('/analytics', authenticate, getUserAnalytics);
 
 export default router;
