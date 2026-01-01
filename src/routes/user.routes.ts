0d154f9111148b222baa0cb8b38ca1b8d7624bdc
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-01-01 22:37:42 +0530

    feat: add /profile update route in user.routes with authentication and image upload

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,13 +1,16 @@
 import { authenticate } from '../middleware/auth.middleware';
-import { getWalletData } from '../controllers/user.controller';
+import { getWalletData, updateProfile } from '../controllers/user.controller';
 import router from './payment.routes';
 import { getDashboardData } from '../controllers/dashboard.controller';
 import { getUserAnalytics } from '../controllers/analytics.controller';
+import { upload } from '../middleware/upload.middleware';
 
 router.get('/wallet', authenticate, getWalletData);
 
 router.get('/dashboard', authenticate, getDashboardData);
 
 router.get('/analytics', authenticate, getUserAnalytics);
+
+router.put('/profile', authenticate, upload.single('image'), updateProfile);
 
 export default router;
