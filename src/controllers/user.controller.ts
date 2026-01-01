3e33e58dd0e2d495a95c870f0cb53e8f783e9dd0
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-01-01 22:37:05 +0530

    feat: update user.controller with updateProfile function

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,6 +5,62 @@ import { Group } from '../model/group.model';
 import { Status as GroupStatus } from '../model/group.model';
 import { Contribution } from '../model/contribution.model';
 import { Status as ContribStatus } from '../model/contribution.model';
+import cloudinary from '../config/cloudinary.config';
+
+export const updateProfile = async (req: AuthRequest, res: Response) => {
+  const userId = req.user.sub;
+  const { firstName, lastName, phone, bankDetails } = req.body;
+
+  try {
+    let profileImageUrl;
+
+    if (req.file) {
+      const result: any = await new Promise(async (resolve, reject) => {
+        const upload_stream = cloudinary.uploader.upload_stream(
+          { folder: 'seettuwa_profiles' },
+          (error, result) => {
+            if (error) {
+              return reject(error);
+            }
+            resolve(result);
+          }
+        );
+        upload_stream.end(req.file?.buffer);
+      });
+      profileImageUrl = result.secure_url;
+    }
+
+    console.log('🔗 profile URL: ', profileImageUrl);
+
+    const updateData: any = {};
+    if (firstName) updateData.firstName = firstName;
+    if (lastName) updateData.lastName = lastName;
+    if (phone) updateData.phone = phone;
+    if (profileImageUrl) updateData.avatarUrl = profileImageUrl;
+
+    console.log(updateData.avatarUrl);
+
+    if (bankDetails) {
+      try {
+        updateData.bankDetails =
+          typeof bankDetails === 'string'
+            ? JSON.parse(bankDetails)
+            : bankDetails;
+      } catch (e) {
+        console.error('Bank details parse error', e);
+      }
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(userId, updateData, {
+      new: true,
+    });
+
+    res.json({ message: 'Profile updated successfully', user: updatedUser });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).send({ error: error.message });
+  }
+};
 
 export const getWalletData = async (req: AuthRequest, res: Response) => {
   const userId = req.user.sub;
