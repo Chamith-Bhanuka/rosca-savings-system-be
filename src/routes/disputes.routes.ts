93fc481a0456271db684b6a141af2b459c846c10
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-01-03 20:40:35 +0530

    feat: update disputes.routes with PUT /resolve route

--- src/routes/disputes.routes.ts
+++ src/routes/disputes.routes.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import { upload } from '../middleware/upload.middleware';
 import { authenticate } from '../middleware/auth.middleware';
-import { getMyDisputes, raiseDispute } from '../controllers/dispute.controller';
+import {
+  getMyDisputes,
+  raiseDispute,
+  resolveDispute,
+} from '../controllers/dispute.controller';
 
 const router = Router();
 
 router.post('/', authenticate, upload.single('image'), raiseDispute);
 
 router.get('/', authenticate, getMyDisputes);
+
+router.put('/resolve', authenticate, resolveDispute);
 
 export default router;
