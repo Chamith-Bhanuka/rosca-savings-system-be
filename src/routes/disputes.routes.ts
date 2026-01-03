21f2d1d38aa92894a07521f9c2c0b1ce86d3be42
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-01-03 20:07:56 +0530

    feat: update disputes.routes with GET / route for getMyDisputes

--- src/routes/disputes.routes.ts
+++ src/routes/disputes.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { upload } from '../middleware/upload.middleware';
 import { authenticate } from '../middleware/auth.middleware';
-import { raiseDispute } from '../controllers/dispute.controller';
+import { getMyDisputes, raiseDispute } from '../controllers/dispute.controller';
 
 const router = Router();
 
 router.post('/', authenticate, upload.single('image'), raiseDispute);
+
+router.get('/', authenticate, getMyDisputes);
 
 export default router;
