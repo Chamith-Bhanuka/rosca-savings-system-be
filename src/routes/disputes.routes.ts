53cd479fcca3e329c704f230c25eceb1164fe9c6
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-01-03 22:14:14 +0530

    feat: update disputes.routes with GET /admin/all route

--- src/routes/disputes.routes.ts
+++ src/routes/disputes.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { upload } from '../middleware/upload.middleware';
 import { authenticate } from '../middleware/auth.middleware';
 import {
+  getAllDisputes,
   getMyDisputes,
   raiseDispute,
   resolveDispute,
@@ -14,5 +15,7 @@ router.post('/', authenticate, upload.single('image'), raiseDispute);
 router.get('/', authenticate, getMyDisputes);
 
 router.put('/resolve', authenticate, resolveDispute);
+
+router.get('/admin/all', authenticate, getAllDisputes);
 
 export default router;
