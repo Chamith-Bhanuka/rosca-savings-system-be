2e2984184477bc2a80ace9c4d0a64601b9726a28
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-01-04 13:16:57 +0530

    feat: create subscriber.model

---
+++ src/model/subscriber.model.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface ISubscriber extends Document {
+  email: string;
+  subscribedAt: Date;
+}
+
+const SubscriberSchema = new Schema<ISubscriber>({
+  email: { type: String, required: true, unique: true, trim: true },
+  subscribedAt: { type: Date, default: Date.now },
+});
+
+export const Subscriber = mongoose.model<ISubscriber>(
+  'Subscriber',
+  SubscriberSchema
+);
