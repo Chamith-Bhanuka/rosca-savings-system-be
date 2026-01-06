afb3ca625b77d4c75057373445a451ed488be034
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-01-07 01:27:28 +0530

    feat: create role.middleware to enforce role-based access

---
+++ src/middleware/role.middleware.ts
@@ -0,0 +1,18 @@
+import { AuthRequest } from './auth.middleware';
+import { Role } from '../model/user.model';
+import { Response, NextFunction } from 'express';
+
+export const authorizeRoles = (...allowedRoles: Role[]) => {
+  return (req: AuthRequest, res: Response, next: NextFunction) => {
+    const userRoles = req.user?.role;
+
+    if (
+      !userRoles ||
+      !userRoles.some((role: Role) => allowedRoles.includes(role))
+    ) {
+      return res.status(403).json({ message: 'Forbidden: Insufficient role' });
+    }
+
+    next();
+  };
+};
